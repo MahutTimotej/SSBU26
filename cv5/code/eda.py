87bc5197eec1c880a7a79bb5836c12b2c8397591
MahutTimotej <mahut@stud.uniza.sk>
2026-04-01 21:27:40 +0200

    Cv5

--- cv5/code/eda.py
+++ cv5/code/eda.py
@@ -1,4 +1,4 @@
-from data_handling_done import Dataset
+from data_handling import Dataset
 
 if __name__ == "__main__":
     dataset = Dataset()
@@ -21,3 +21,16 @@ if __name__ == "__main__":
     # plot pair plot for the first 5 features
     selected_features = dataset.feature_names[:5]
     dataset.plot_pair_plot(selected_features)
+
+    stats = dataset.calculate_statistics()
+    print("Stats:")
+    print(stats)
+
+    summary_all = dataset.summarize_features()
+    print("Summary all:")
+    print(summary_all)
+
+    selected = ["mean radius", "mean texture", "mean area"]
+    summary_selected = dataset.summarize_features(feature_names=selected)
+    print("Summary selected:")
+    print(summary_selected)
